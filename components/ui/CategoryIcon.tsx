764005563b5e6a8cfeb1f0da3a6f0b6e89715da3
Byron Francisco Chanax Itzep <1byron1999@gmail.com>
2024-07-18 17:57:22 -0700

    add link selected

--- components/ui/CategoryIcon.tsx
+++ components/ui/CategoryIcon.tsx
@@ -1,14 +1,18 @@
+"use client"
 import { Category } from '@prisma/client';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-
+import { useParams } from 'next/navigation';
 interface CategoryProps {
     category: Category
-}
+};
+
 const CategoryIcon = ({ category }: CategoryProps) => {
+    const params = useParams<{category : string}>();
+   
     return (
-        <div className={`flex items-center gap-4 w-full border-t border-gray-200 last-of-type:border-b p-3`}>
+        <div className={`${ category.slug == params.category ? 'bg-amber-400' : ''} flex items-center gap-4 w-full border-t border-gray-200 last-of-type:border-b p-3`}>
             <div className='w-16 h-16 relative'>
                 <Image
                     src={`/icon_${category.slug}.svg`}
