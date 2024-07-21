84ae408d9c56bb774b3f87002165895d6dc17d95
Byron Francisco Chanax Itzep <1byron1999@gmail.com>
2024-07-21 06:38:44 -0700

    show products in admin page

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,12 +1,24 @@
+import ProductTable from '@/components/products/ProductsTable';
 import Heading from '@/components/ui/Heading'
-import React from 'react'
+import { prisma } from '@/src/lib/prisma';
 
-const page = () => {
+const getProducts = async () => {
+    const products = await prisma.product.findMany({
+        include: { category: true }
+    });
+    return products
+};
+
+export type ProductWithCategory = Awaited<ReturnType<typeof getProducts>>;
+
+const page = async () => {
+    const products = await getProducts()
     return (
         <>
-        <Heading>
-            Adminstrar tus productos
-        </Heading>
+            <Heading>
+                Adminstrar tus productos
+            </Heading>
+            <ProductTable products={products} />
         </>
     )
 }
