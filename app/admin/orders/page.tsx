bec4d48611c75e5c07904e4ac584749956820e7a
Byron Francisco Chanax Itzep <1byron1999@gmail.com>
2024-07-21 05:41:38 -0700

    add order card produt in admin page

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,14 +1,41 @@
+import OrderCard from '@/components/admin/OrderCard';
 import Heading from '@/components/ui/Heading'
+import { prisma } from '@/src/lib/prisma'
 import React from 'react'
 
-const pages = () => {
+const getOrders = async () => {
+  const orders = await prisma.order.findMany({
+    where: {
+      status: false
+    },
+    include: {
+      orderProducts: {
+        include: { product: true }
+      }
+    }
+  });
+
+  return orders
+}
+
+const OrdersPage = async () => {
+  const orders = await getOrders();
+
   return (
-    <div>
+    <>
       <Heading>
         Administra tus ordenes
       </Heading>
-    </div>
+      {orders.length ?
+        <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5 mt-5 '>
+          {orders.map(order => (
+            <OrderCard order={order} key={order.id} />
+          ))}
+        </div> :
+        <p className='text-center'>Aún no tienes ordenes </p>}
+
+    </>
   )
 }
 
-export default pages
+export default OrdersPage
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,12 @@
-import { Product } from "@prisma/client";
+import { Order, OrderProducts, Product } from "@prisma/client";
 
 export type OrderType = Pick<Product, 'id' | 'name' | 'price'> & {
     quantity: number,
     subtotal: number
+};
+
+export type OrderWithProducts = Order & {
+    orderProducts: (OrderProducts & {
+        product: Product
+    })[]
 }
