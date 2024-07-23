e6e9cabe769a57bdf45b761c68929cf0fbcb58b9
Byron Francisco Chanax Itzep <1byron1999@gmail.com>
2024-07-23 14:54:55 -0700

    add swr refresh data

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,41 +1,39 @@
+"use client"
 import OrderCard from '@/components/admin/OrderCard';
 import Heading from '@/components/ui/Heading'
-import { prisma } from '@/src/lib/prisma'
+import useSWR from 'swr';
 import React from 'react'
+import { OrderWithProducts } from '@/src/types';
 
-const getOrders = async () => {
-  const orders = await prisma.order.findMany({
-    where: {
-      status: false
-    },
-    include: {
-      orderProducts: {
-        include: { product: true }
-      }
-    }
+const OrdersPage = () => {
+
+  const url = '/admin/orders/api';
+  const fetcher = () => fetch(url)
+    .then(res => res.json())
+    .then(data => data)
+
+  const { data: orders, isLoading, error } = useSWR<OrderWithProducts[]>(url, fetcher, {
+    refreshInterval: 60000,
+    revalidateOnFocus: false
   });
 
-  return orders
-}
+  if(isLoading) return 'loading...'
+  if (orders)
+    return (
+      <>
+        <Heading>
+          Administra tus ordenes
+        </Heading>
+        {orders.length ?
+          <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5 mt-5 '>
+            {orders.map(order => (
+              <OrderCard order={order} key={order.id} />
+            ))}
+          </div> :
+          <p className='text-center'>Aún no tienes ordenes </p>}
 
-const OrdersPage = async () => {
-  const orders = await getOrders();
-
-  return (
-    <>
-      <Heading>
-        Administra tus ordenes
-      </Heading>
-      {orders.length ?
-        <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5 mt-5 '>
-          {orders.map(order => (
-            <OrderCard order={order} key={order.id} />
-          ))}
-        </div> :
-        <p className='text-center'>Aún no tienes ordenes </p>}
-
-    </>
-  )
+      </>
+    )
 }
 
 export default OrdersPage
